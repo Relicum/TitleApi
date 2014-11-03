47bee0d59d35787cf06b357148e8f53eb1dd6e16
Relicum <chrislutte@relicum.com>
2014-11-03 14:37:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+##TitleApi
+```
+REQUIRES
+* Latest Patch Spigot
+* Java 8 installed (Server Side)
+```
+
+TitleApi gives developers easy access to the following (MC)1.8 features:
+
+* Title
+* SubTitle
+* Tab Header
+* Tab Footer
+* Action Bar
+---
+
+All plugins that wish to use this must make sure there plugin.yml includes a dependency to TitleApi
---
+++ src/main/java/com/relicum/titleapi/TitleApi.java
@@ -0,0 +1,43 @@
+/*
+ * TitleApi is a development API for Minecraft Titles and Tabs, developed by Relicum
+ * Copyright (C) 2014.  Chris Lutte
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.relicum.titleapi;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Name: TitleApi.java Created: 03 November 2014
+ *
+ * @author Relicum
+ * @version 0.0.1
+ */
+public class TitleApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "titleapi is being installed");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
